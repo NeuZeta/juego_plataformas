f01a0be7dc61c380b39001664593b7d7e144a34c
NeusRB <neuzeta@gmail.com>
2018-09-24 19:26:52 +0200

    PlayerScript + gameplay map colliders

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float velocidadRotacion = 50;
+    public float velocidadLineal = 1f;
+    Rigidbody2D rigidbody;
+
+
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();	
+	}
+	
+    public void MoveRight()
+    {
+        //rigidbody.velocity += new Vector2(transform.right.x * velocidadLineal, transform.right.y * velocidadLineal) * Time.deltaTime;      
+        rigidbody.velocity += new Vector2(transform.right.x * velocidadLineal, 0f);
+
+    }
+
+    public void MoveLeft()
+    {
+        rigidbody.velocity -= new Vector2(transform.right.x * velocidadLineal, 0f);
+    }
+
+    public void RotateRight()
+    {
+        rigidbody.MoveRotation(rigidbody.rotation - velocidadRotacion * Time.deltaTime);
+    }
+
+    public void RotateLeft()
+    {
+        rigidbody.MoveRotation(rigidbody.rotation + velocidadRotacion * Time.deltaTime);
+    }
+
+ 
+    void Update () {
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            RotateLeft();
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            RotateRight();
+        }
+
+	}
+}
